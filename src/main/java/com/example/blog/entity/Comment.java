89f9dbf6b90e2f9748f0377a05b7d775934b6a80
Mike Lee <mikelee3@gmail.com>
2024-10-19 20:31:10 -0500

    Add User, Post, and Comment, as well as sql script to install them, and application.yml config for db connectivity.

---
+++ src/main/java/com/example/blog/entity/Comment.java
@@ -0,0 +1,17 @@
+package com.example.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+}
---
+++ src/main/java/com/example/blog/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+}
